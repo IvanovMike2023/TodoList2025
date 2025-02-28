e30b9bcb9c6845eb0066c16c57be54de5bf13138
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2025-02-28 18:01:16 +0300

    letsStartcasca

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React, {useRef} from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import s from './app.module.css'
 import './App.css';
-import {Button, IconButton, TextField} from "@mui/material";
-import Icon from '@mui/material/Icon';
-import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
-import {inspect} from "util";
 import {Todolist} from "./Components/Todolist";
 import {AddItem} from "./Components/AddItem";
-
+export type Tasks=Task[]
+type Task= {
+    id: number
+    title: string
+    isdone:boolean
+}
 function App() {
-    const ref = useRef<HTMLInputElement | null>(null)
+   const deleteTask=()=>{
+       console.log('dddd')
+   }
 
-    const handleClick = (e: React.MouseEvent): void => {
-        if (ref.current !== null) console.log(ref.current.value);
-    };
+
+    const tasks:Tasks=[
+        {id:1,title:'xsax',isdone:true},
+        {id:2,title:'2222',isdone:true},
+        {id:3,title:'3333',isdone:true}
+    ]
     return (
         <div className={s.AppContainer}>
             <div className={s.HeadWrapper}>
@@ -29,8 +34,7 @@ function App() {
                 <AddItem/>
             </div>
             <div className={s.Container}>
-                <Todolist title={'casac'}/>
-                <Todolist title={'111111'}/>
+                <Todolist deleteTask={deleteTask} title={'casac'} tasks={tasks}/>
             </div>
         </div>
     );
--- src/Components/Todolist.tsx
+++ src/Components/Todolist.tsx
@@ -1,12 +1,17 @@
 import {AddItem} from "./AddItem";
 import {Box, IconButton, Paper} from "@mui/material";
-import AddBoxOutlinedIcon from "@mui/icons-material/AddBoxOutlined";
+import DeleteIcon from "@mui/icons-material/Delete";
 import React from "react";
 import s from './todolist.module.css'
+import {Tasks} from "../App";
 type Props={
-    title:string
+    title:string,
+    deleteTask:()=>void
+    tasks:Tasks
 }
 export const Todolist=(props:Props)=>{
+
+
     return <>
         <Box
             sx={{
@@ -15,16 +20,21 @@ export const Todolist=(props:Props)=>{
                 '& > :not(style)': {
                     m: 1,
                     width: 228,
-                    height: 228,
+                    height: 328,
                 },
             }}
         >
             <Paper elevation={3} >
                 <div className={s.TitleTodoList}>
                 <p>{props.title}</p>
-                <IconButton color="primary"><AddBoxOutlinedIcon color={"primary"}/></IconButton>
+                <IconButton color="primary"><DeleteIcon color={'action'}/></IconButton>
                 </div>
                     <AddItem/>
+                <ul>
+                {props.tasks.map((el)=>{
+                   return <li> {el.title} <IconButton color="primary"><DeleteIcon onClick={props.deleteTask} color={'action'}/></IconButton></li>
+                })}
+                </ul>
             </Paper>
         </Box>
     </>
