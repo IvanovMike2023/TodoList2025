ea5be46faa0366eafdd975f21e7eb62c0e5e53a8
IvanovMike2023 <ivanov.michail.2023@yandex.ru>
2025-07-01 22:16:24 +0300

    paginationауыусфысыф

--- src/features/todolists/ui/TodoLists/TodoListItem/Tasks/TaskItem/TaskItem.tsx
+++ src/features/todolists/ui/TodoLists/TodoListItem/Tasks/TaskItem/TaskItem.tsx
@@ -23,6 +23,7 @@ type Props = {
     lengthItemAr:number
 }
 
+
 export const TaskItem = ({ task, todolist,page ,setPage,lengthItemAr}: Props) => {
 const [deleteTask]=useDeleteTaskMutation()
     const [updateTask]=useUpdateTaskMutation()
